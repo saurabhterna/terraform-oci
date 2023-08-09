61de5dab15648e651912a8b861931ab6539302b0
saurabhterna <saurabh.terna@gmail.com>
2023-08-09 10:14:16 +0530

    Update provider.tf

--- Module-1/provider.tf
+++ Module-1/provider.tf
@@ -2,6 +2,6 @@ provider "oci" {
   tenancy_ocid   = "ocid1.tenancy.oc1..aaaaaaaaxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx2hna"
   user_ocid      = "ocid1.user.oc1..aaaaaaaavxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxca"
   fingerprint      = "34:70:fc:XX2:54:2b:f8:58:6f:xx:xx:xx:de:xx:xx:ca"
-  private_key_path = "/home/sample/.oci/id_user.pem" # Replace with your directory location stored .pem file
+  private_key_path = "~/.oci/private_file_name.pem" # Replace with your directory location stored .pem file
   region           = "us-phoenix-1" # Replace with your desired region
 }
