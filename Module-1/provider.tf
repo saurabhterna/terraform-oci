990060bec9afefa7b80fb5b1b07e1f80fef58096
saurabhterna <saurabh.terna@gmail.com>
2023-08-09 09:07:23 +0530

    Create provider.tf

---
+++ Module-1/provider.tf
@@ -0,0 +1,7 @@
+provider "oci" {
+  tenancy_ocid   = "ocid1.tenancy.oc1..aaaaaaaaxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx2hna"
+  user_ocid      = "ocid1.user.oc1..aaaaaaaavxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxca"
+  fingerprint    = "35:69:fc:20:54:2b:f8:58:6f:51:f7:a9:de:3e:a8:ca"
+  private_key_path = "/home/saurabh_d_/.oci/id_sau.pem"
+  region         = "us-phoenix-1"
+}
