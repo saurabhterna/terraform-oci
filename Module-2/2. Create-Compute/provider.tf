75a361c1f8504c53131aa0b1990dae40b6bc1948
saurabhterna <saurabh.terna@gmail.com>
2023-08-11 14:39:37 +0530

    Update provider.tf

--- Module-2/2. Create-Compute/provider.tf
+++ Module-2/2. Create-Compute/provider.tf
@@ -1,7 +1,7 @@
 provider "oci" {
-  tenancy_ocid   = "ocid1.tenancy.oc1..aaaaaaaaxy6bh46cdnlfpaibasc6dotowv32hc2sbj4ph3ocxtfxhhva2hna"
-  user_ocid      = "ocid1.user.oc1..aaaaaaaavx2gmkyujjrariybblythrc7qyhzlb5mka53oxx3q2iq2sujvjca"
-  fingerprint    = "35:69:fc:20:54:2b:f8:58:6f:51:f7:a9:de:3e:a8:ca"
-  private_key_path = "/home/saurabh_d_/.oci/id_sau.pem"
+  tenancy_ocid   = "ocid1.tenancy.oc1..aaaaaaaaxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+  user_ocid      = "ocid1.user.oc1..aaaaaaaavxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+  fingerprint    = "35:69:fc:20:54:2b:xx:xx:6f:51:f7:XX:xx:xx:xx:xx"
+  private_key_path = "~/.oci/id_rsa.pem"
   region         = "us-phoenix-1"
 }
