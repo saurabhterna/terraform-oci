cf7682d6c2dc69e1ea05512e022eb38d483cf3a9
saurabhterna <saurabh.terna@gmail.com>
2023-08-11 14:35:53 +0530

    Delete provider.tf

--- provider.tf
+++
@@ -1,7 +0,0 @@
-provider "oci" {
-  tenancy_ocid   = "ocid1.tenancy.oc1..aaaaaaaaxy6bh46cdnlfpaibasc6dotowv32hc2sbj4ph3ocxtfxhhva2hna"
-  user_ocid      = "ocid1.user.oc1..aaaaaaaavx2gmkyujjrariybblythrc7qyhzlb5mka53oxx3q2iq2sujvjca"
-  fingerprint    = "35:69:fc:20:54:2b:f8:58:6f:51:f7:a9:de:3e:a8:ca"
-  private_key_path = "/home/saurabh_d_/.oci/id_sau.pem"
-  region         = "us-phoenix-1"
-}
