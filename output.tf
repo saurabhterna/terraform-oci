bb00537a45ff765596474089b60cd8460c68a4a5
saurabhterna <saurabh.terna@gmail.com>
2023-08-25 17:20:00 +0530

    Delete output.tf

--- output.tf
+++
@@ -1,9 +0,0 @@
-output "users_temp_credentials" {
-  sensitive = false
-  value     = "${formatlist("User: %v   Password: %v  ", oci_identity_user.ocilab-user.name, oci_identity_ui_password.ocilab-password.password)}"
-  #value = "${join(" \n ",oci_identity_ui_password.user_pwd.*.password)}"
-}
-
-output "tenancy_link" {
-  value = "https://https://cloud.oracle.com/?tenant=${data.oci_identity_tenancy.current_tenancy.name}"
-}
