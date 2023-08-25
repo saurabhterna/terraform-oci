340c632a96da966d52e0a3236a42549d662d17c5
saurabhterna <saurabh.terna@gmail.com>
2023-08-25 17:30:10 +0530

    Update provider.tf

--- Module-2/4. Create an OCI Cloud Lab/provider.tf
+++ Module-2/4. Create an OCI Cloud Lab/provider.tf
@@ -1,8 +1,8 @@
 provider "oci" {
-  tenancy_ocid   = "ocid1.tenancy.oc1..aaaaaaaaxy6bh46cdnlfpaibasc6dotowv32hc2sbj4ph3ocxtfxhhva2hna"
-  user_ocid      = "ocid1.user.oc1..aaaaaaaavx2gmkyujjrariybblythrc7qyhzlb5mka53oxx3q2iq2sujvjca"
-  fingerprint    = "b6:7d:c5:57:a4:7a:4e:fc:16:92:b1:88:a3:04:4c:95"
-  private_key_path = "/Users/saurapat/Desktop/labsetuo/id_sau.pem"
+  tenancy_ocid   = "ocid1.tenancy.oc1..aaaaaaaaxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxa"
+  user_ocid      = "ocid1.user.oc1..aaaaaaaxxxxxxxxxxxxxxxxxxxxxxxxxxyxxxxxxxxxxxxxxxxxxxxxxxxxxsujvjca"
+  fingerprint    = "b6:7d:c5:57:a4:7a:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx"
+  private_key_path = "~/id_user.pem"
   region         = "us-phoenix-1"
 }
 
